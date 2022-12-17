bc5fb0e9df0ce69c8bbb10d79b0de6a77069f494
Scholtz <ludko2@gmail.com>
2022-12-17 21:55:09 +0100

    rename TinyManStakingBot to AMMStakingBot, fix cicd

--- compose.sh
+++ compose.sh
@@ -1,4 +1,17 @@
+if [ "$ver" == "" ]; then
 ver=1.0.0
-docker build -t "scholtz2/tinyman-staking-bot:$ver-stable" -f TinyManStakingBot/Dockerfile  ./
-docker push "scholtz2/tinyman-staking-bot:$ver-stable"
-echo "Image: scholtz2/tinyman-staking-bot:$ver-stable"
+fi
+
+docker build -t "scholtz2/algorand-amm-staking-bot:$ver-stable" -f AMMStakingBot/Dockerfile  ./ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "docker build failed";
+	exit 1;
+fi
+docker push "scholtz2/algorand-amm-staking-bot:$ver-stable" || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "docker push failed";
+	exit 1;
+fi
+echo "Image: scholtz2/algorand-amm-staking-bot:$ver-stable"
